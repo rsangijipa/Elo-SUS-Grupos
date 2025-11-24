65e42168ee3ea513793c667e6b6554eb6b1067e4
Richard Sangi <doll.ricardoll@gmail.com>
2025-11-24 01:04:11 -0400

    v1.4.1.1

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css' // Garanta que este arquivo tenha as diretivas @tailwind
+import './index.css'
+import { setupDevEnvironment } from './services/devSeed';
+
+// Run seeding in development
+setupDevEnvironment();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
